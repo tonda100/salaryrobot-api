f1496ede0d1b35f9ef6ad95e1e28f6e005cabbc5
Antonin Stoklasek <antonin.stoklasek@bsc-ideas.com>
2018-02-06 22:54:45 +0100

    added precision for exchange pairs

--- src/main/java/com/salaryrobot/api/exchange/entity/ExchangePair.java
+++ src/main/java/com/salaryrobot/api/exchange/entity/ExchangePair.java
@@ -7,32 +7,38 @@ package com.salaryrobot.api.exchange.entity;
  * @author Antonin Stoklasek
  */
 public enum ExchangePair {
-    BTC_USD(Commodity.BTC, PaymentCurrency.USD),
-    BTC_EUR(Commodity.BTC, PaymentCurrency.EUR),
-    EUR_USD(Commodity.EUR, PaymentCurrency.USD),
-    XRP_USD(Commodity.XRP, PaymentCurrency.USD),
-    XRP_EUR(Commodity.XRP, PaymentCurrency.EUR),
-    XRP_BTC(Commodity.XRP, PaymentCurrency.BTC),
-    LTC_USD(Commodity.LTC, PaymentCurrency.USD),
-    LTC_EUR(Commodity.LTC, PaymentCurrency.EUR),
-    LTC_BTC(Commodity.LTC, PaymentCurrency.BTC),
-    ETH_USD(Commodity.ETH, PaymentCurrency.USD),
-    ETH_EUR(Commodity.ETH, PaymentCurrency.EUR),
-    ETH_BTC(Commodity.ETH, PaymentCurrency.BTC),
-    BCH_USD(Commodity.BCH, PaymentCurrency.USD),
-    BCH_EUR(Commodity.BCH, PaymentCurrency.EUR),
-    BCH_BTC(Commodity.BCH, PaymentCurrency.BTC);
+    BTC_USD(Commodity.BTC, 8, PaymentCurrency.USD, 2),
+    BTC_EUR(Commodity.BTC, 8, PaymentCurrency.EUR, 2),
+    EUR_USD(Commodity.EUR, 5, PaymentCurrency.USD, 5),
+    XRP_USD(Commodity.XRP, 8, PaymentCurrency.USD, 5),
+    XRP_EUR(Commodity.XRP, 8, PaymentCurrency.EUR, 5),
+    XRP_BTC(Commodity.XRP, 8, PaymentCurrency.BTC, 8),
+    LTC_USD(Commodity.LTC, 8, PaymentCurrency.USD, 2),
+    LTC_EUR(Commodity.LTC, 8, PaymentCurrency.EUR, 2),
+    LTC_BTC(Commodity.LTC, 8, PaymentCurrency.BTC, 8),
+    ETH_USD(Commodity.ETH, 8, PaymentCurrency.USD, 2),
+    ETH_EUR(Commodity.ETH, 8, PaymentCurrency.EUR, 2),
+    ETH_BTC(Commodity.ETH, 8, PaymentCurrency.BTC, 8),
+    BCH_USD(Commodity.BCH, 8, PaymentCurrency.USD, 2),
+    BCH_EUR(Commodity.BCH, 8, PaymentCurrency.EUR, 2),
+    BCH_BTC(Commodity.BCH, 8, PaymentCurrency.BTC, 8);
 
     private Commodity commodity;
 
+    private int commodityPrecision;
+
     private PaymentCurrency paymentCurrency;
+
+    private int paymentPrecision;
 
     private String code;
 
-    ExchangePair(Commodity commodity, PaymentCurrency paymentCurrency) {
+    ExchangePair(Commodity commodity, int commodityPrecision, PaymentCurrency paymentCurrency, int paymentPrecision) {
         this.commodity = commodity;
         this.paymentCurrency = paymentCurrency;
         this.code = commodity.getCode() + paymentCurrency.getCode();
+        this.commodityPrecision = commodityPrecision;
+        this.paymentPrecision = paymentPrecision;
     }
 
     public Commodity getCommodity() {
@@ -45,6 +51,14 @@ public enum ExchangePair {
 
     public String getCode() {
         return code;
+    }
+
+    public int getCommodityPrecision() {
+        return commodityPrecision;
+    }
+
+    public int getPaymentPrecision() {
+        return paymentPrecision;
     }
 
     public static ExchangePair valueFromCode(String code) {
